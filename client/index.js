37d9dbf6a98b95cbe682773c47d6c870c8a501bf
jcovington16 <covingtonjoshua7@gmail.com>
2023-05-21 17:52:03 -0600

    chore: adding verification logic to server.

---
+++ client/index.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+const niceList = require('../utils/niceList.json');
+const MerkleTree = require('../utils/MerkleTree');
+
+const serverUrl = 'http://localhost:1225';
+
+async function main() {
+  // TODO: how do we prove to the server we're on the nice list? 
+  const merkle = new MerkleTree(niceList)
+  const root = merkle._getRoot(); 
+
+  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+    // TODO: add request body parameters here!
+  });
+
+  console.log({ gift });
+}
+
+main();
